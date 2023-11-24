f498802d43a5c328d0df70e52b9d0f77b9aa15c1
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-11-24 10:25:02 +0000

    added reset password and forgotten password endpoints

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,18 +1,35 @@
-const express = require('express')
-const { createUser, login, getAllUser, getUser, deleteUser, updateUser, blockUser, unBlockUser, handleRefreshToken, logout } = require("../controller/userController")
-const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
-const router = express.Router()
+const express = require("express");
+const {
+  createUser,
+  login,
+  getAllUser,
+  getUser,
+  deleteUser,
+  updateUser,
+  blockUser,
+  unBlockUser,
+  handleRefreshToken,
+  logout,
+  updatePassword,
+  forgotPasswordToken, 
+  resetPassword
+} = require("../controller/userController");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
+const router = express.Router();
 
-router.post("/register", createUser)
-router.post("/login", login)
-router.get("/logout", logout)
-router.get("/refresh-token", handleRefreshToken)
+router.post("/register", createUser);
+router.post("/login", login);
+router.get("/logout", logout);
+router.get("/refresh-token", handleRefreshToken);
+router.put("/update-password", authMiddleware, updatePassword);
+router.post("/forgot-password", forgotPasswordToken);
+router.post("/reset-password/:token", resetPassword);
 
-router.get("/all-users", getAllUser)
-router.get("/:id", authMiddleware, isAdmin, getUser)
-router.delete("/:id", deleteUser)
-router.put("/edit", authMiddleware, updateUser)
-router.put("/block-user/:id", authMiddleware, isAdmin, blockUser)
-router.put("/unblock-user/:id", authMiddleware, isAdmin, unBlockUser)
+router.get("/all-users", getAllUser);
+router.get("/:id", authMiddleware, isAdmin, getUser);
+router.delete("/:id", deleteUser);
+router.put("/edit", authMiddleware, updateUser);
+router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware, isAdmin, unBlockUser);
 
-module.exports = router
+module.exports = router;
