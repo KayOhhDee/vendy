984f79ac208bef49288ebb70ef8eac5d831b830c
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-12-01 14:09:05 +0000

    add product category CRUD

---
+++ controller/productCategoryController.js
@@ -0,0 +1,64 @@
+const Category = require("../models/productCategoryModel");
+const asyncHandler = require("express-async-handler");
+const validateMongoDBId = require("../utils/validateMongoDBID");
+
+const createCategory = asyncHandler(async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.json(category);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const updateCategory = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    validateMongoDBId(id);
+    const category = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(category);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const deleteCategory = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    validateMongoDBId(id);
+    const category = await Category.findByIdAndDelete(id);
+    res.json(category);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getCategory = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    validateMongoDBId(id);
+    const category = await Category.findById(id);
+    res.json(category);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getCategories = asyncHandler(async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategory,
+  getCategories
+};
--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const dbConnect = require('./config/dbConnect');
-const dotenv = require('dotenv').config()
-const app = express()
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
-const morgan = require('morgan')
-const PORT = process.env.PORT || 4000
+const express = require("express");
+const dbConnect = require("./config/dbConnect");
+const dotenv = require("dotenv").config();
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
-const { notFound, errorHandler } = require('./middlewares/errorHandler');
-dbConnect()
+const categoryRouter = require("./routes/productCategoryRoute");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
+dbConnect();
 
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use("/api/user", authRouter)
-app.use("/api/product", productRouter)
-app.use("/api/blog", blogRouter)
+app.use("/api/user", authRouter);
+app.use("/api/product", productRouter);
+app.use("/api/blog", blogRouter);
+app.use("/api/product-category", categoryRouter)
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log('Server is running at PORT', PORT)
-})
+  console.log("Server is running at PORT", PORT);
+});
